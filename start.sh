25fa0a9e8b459487303c9fbc8b4b1a8247209e7b
fpaindorge <florian.paindorge@arkena.com>
2016-01-27 14:24:09 +0100

    Improve regexp for hello script

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ export RESPOND_TO_DM=true
 export PORT=8082
 export HUBOT_ANSWER='42'
 export HUBOT_BUSINESS_CAT_JARGON='refactor,rails4,gardening,cloud'
+export HUBOT_BUSINESS_CAT_OMITTED_JARGON='manifest'
 export HUBOT_GIPHY_API_KEY='dc6zaTOxFJmzC'
 export HUBOT_ANNOUNCE_ROOMS='QwyoxgTuxzqPJuLqR'
 
