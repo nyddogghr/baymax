52e22fb35a45a3b89cda0dc62b6e7e9ad7af75ff
fpaindorge <florian.paindorge@arkena.com>
2016-01-27 09:44:17 +0100

    Update location decision maker doc

--- start.sh
+++ start.sh
@@ -9,8 +9,6 @@ export ROCKETCHAT_PASSWORD=baymax
 export PORT=8082
 export HUBOT_ANSWER='42'
 export HUBOT_BUSINESS_CAT_JARGON='refactor,rails4,gardening,cloud'
-export HUBOT_GOOGLE_CSE_ID='015105712699671892457:s5dlmzbteyc'
-export HUBOT_GOOGLE_CSE_KEY='AIzaSyA4PUvC6r7WVJktDXoVqo6WLjf1zMEPBiU'
 export HUBOT_GIPHY_API_KEY='dc6zaTOxFJmzC'
 export HUBOT_ANNOUNCE_ROOMS='QwyoxgTuxzqPJuLqR'
 
