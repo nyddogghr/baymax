05414026ae1501767744a7ae4a16096c3bd6b4ea
nyddogghr <nyddogghr@gmail.com>
2016-09-26 14:02:49 +0200

    Remove business cat

--- start.sh
+++ start.sh
@@ -6,8 +6,6 @@ export LISTEN_ON_ALL_PUBLIC=false
 export RESPOND_TO_DM=true
 export PORT=8082
 export HUBOT_ANSWER='42'
-export HUBOT_BUSINESS_CAT_JARGON='refactor,rails4,gardening,cloud'
-export HUBOT_BUSINESS_CAT_OMITTED_JARGON='manifest'
 export HUBOT_GIPHY_API_KEY='dc6zaTOxFJmzC'
 export HUBOT_ANNOUNCE_ROOMS='QwyoxgTuxzqPJuLqR'
 
